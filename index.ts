df9b5dbb8b7224b7d0b5255f9c99d74635539040
Sora Daibu <soradaibu@Soras-MacBook-Pro.local>
2024-07-30 13:59:01 +0200

    pimlico tutorial!

--- index.ts
+++ index.ts
@@ -1,36 +1,77 @@
 import "dotenv/config"
 import { writeFileSync } from 'fs'
 import {
-    ENTRYPOINT_ADDRESS_V07, 
-    GetUserOperationReceiptReturnType, 
-    UserOperation,
-    bundlerActions, 
-    createSmartAccountClient, 
-    getAccountNonce, 
-    getSenderAddress, 
-    getUserOperationHash, 
-    signUserOperationHashWithECDSA, 
-    waitForUserOperationReceipt
+    ENTRYPOINT_ADDRESS_V07,
+    createSmartAccountClient,
 } from "permissionless"
-import { 
-    privateKeyToSafeSmartAccount, 
-    privateKeyToSimpleSmartAccount, 
-    signerToSafeSmartAccount 
+import {
+    signerToSafeSmartAccount
 } from "permissionless/accounts"
-import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico"
 import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from "permissionless/clients/pimlico"
-import { 
-    Address, 
-    Hash, 
-    Hex, 
-    concat, 
-    createClient, 
-    createPublicClient, 
-    encodeFunctionData, 
-    http, 
-    parseAbiItem 
+import {
+    Hex,
+    createPublicClient,
+    http,
 } from "viem"
-import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
-import { lineaTestnet, polygonMumbai, sepolia } from "viem/chains"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { sepolia } from "viem/chains"
 
 console.log("Hello world!")
+
+const apiKey = "xx"
+const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+
+const privateKey =
+	(process.env.PRIVATE_KEY as Hex) ??
+	(() => {
+		const pk = generatePrivateKey()
+		writeFileSync(".env", `PRIVATE_KEY=${pk}`)
+		return pk
+	})()
+
+export const publicClient = createPublicClient({
+	transport: http("https://rpc.ankr.com/eth_sepolia"),
+})
+
+export const paymasterClient = createPimlicoPaymasterClient({
+	transport: http(paymasterUrl),
+	entryPoint: ENTRYPOINT_ADDRESS_V07,
+})
+
+const account = await signerToSafeSmartAccount(publicClient, {
+	signer: privateKeyToAccount(privateKey),
+	entryPoint: ENTRYPOINT_ADDRESS_V07, // global entrypoint
+	safeVersion: "1.4.1",
+})
+
+console.log(`Smart account address: https://sepolia.etherscan.io/address/${account.address}`)
+
+const bundlerUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+
+const bundlerClient = createPimlicoBundlerClient({
+	transport: http(bundlerUrl),
+	entryPoint: ENTRYPOINT_ADDRESS_V07,
+})
+
+const smartAccountClient = createSmartAccountClient({
+	account,
+	entryPoint: ENTRYPOINT_ADDRESS_V07,
+	chain: sepolia,
+	bundlerTransport: http(bundlerUrl, {
+		timeout: 30_000 // Wait 30 seconds for user operation to be included
+	}),
+	middleware: {
+		gasPrice: async () => {
+			return (await bundlerClient.getUserOperationGasPrice()).fast
+		},
+		sponsorUserOperation: paymasterClient.sponsorUserOperation,
+	},
+})
+
+const txHash = await smartAccountClient.sendTransaction({
+	to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+	value: 0n,
+	data: "0x1234",
+})
+
+console.log(`User operation included: https://sepolia.etherscan.io/tx/${txHash}`)
